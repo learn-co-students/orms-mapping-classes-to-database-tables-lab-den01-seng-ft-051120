45e6f0da554b4b65a47bdeeb46ffe8cb5a4ceb42
github username <github email address>
2020-05-20 17:15:06 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,42 @@
-class Student
+# frozen_string_literal: true
 
+class Student
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  def initialize(name, grade, _id = nil)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE students')
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, @name, @grade)
+    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+  end
 end
