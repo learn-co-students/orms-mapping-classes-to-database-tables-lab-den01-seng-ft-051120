2d3fdbc54defe78b9b7bd4fe39bc6284069ab427
github username <github email address>
2020-05-23 17:00:35 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,6 @@ require 'bundler'
 Bundler.require
 
 require_relative '../lib/student'
-
+#below code sets up a data for students in a public school district in SQL
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+#refer to database with the command DB[:conn]
